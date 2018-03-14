8bd8650def3a169fb5bce19b702e0ca59081d146
Veda Sindhoora <drsindhoo@gmail.com>
2018-03-14 20:47:41 +0530

    modified the module to war module.added junit 5 support. TODO : selenium test for index.html

--- get-start/src/test/java/com/vedz/selenium/AppTest.java
+++ get-start/src/test/java/com/vedz/selenium/AppTest.java
@@ -1,38 +1,19 @@
 package com.vedz.selenium;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class AppTest {
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    @Test
+    public void test()
     {
-        assertTrue( true );
+        Assertions.assertTrue( true );
     }
 }
